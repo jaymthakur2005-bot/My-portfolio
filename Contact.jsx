d751854c5eeb1cffee6b03a18a0abb7ea3733975
Jay Thakur <thakurmjay20052gmail.com>
2025-09-06 00:57:08 -0400

    Deploy build files

--- Contact.jsx
+++ Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import RevealOnScroll from "../RevealOnScroll";
+import RevealOnScroll from "./src/Components/RevealOnScroll";
 import emailjs from "emailjs-com";
 
 const Contact = () => {
